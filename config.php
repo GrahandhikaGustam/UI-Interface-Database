bafcb933a5298ea6d379e998f0aa1020bf2f1ad1
GrahandhikaGustam <gustamagrahandhikaemail.com@gmail.com>
2020-11-27 19:35:08 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseUsername = 'root';
+$databasePassword = '';
+$databaseName ='crud_db';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+?>
